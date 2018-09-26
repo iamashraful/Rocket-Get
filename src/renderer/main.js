48641ba7ce76725aff9fdccb900f77b50eebb648
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-09-26 22:07:15 +0600

    * Added navbar component to showing navigation bar * Added font awesome and make it for global component

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,9 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.config.productionTip = false
+library.add(faCoffee);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,4 +20,4 @@ new Vue({
   router,
   store,
   template: '<App/>'
-}).$mount('#app')
+}).$mount('#app');
--- src/renderer/views/TopNavbar.vue
+++ src/renderer/views/TopNavbar.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="top-navbar">
-        <h3>Top Navbar</h3>
+        <!-- test code will remove soon-->
+        <a class="active" href="#home"><font-awesome-icon icon="coffee" /></a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
     </div>
 </template>
 
@@ -14,5 +18,23 @@
     .top-navbar {
         margin-left: 157px;
         padding-top: 5px;
+        overflow: hidden;
+
+        a {
+            float: left;
+            color: #020117;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+            &:hover {
+                background-color: #ddd;
+                color: black;
+            }
+            &:active {
+                background-color: #4CAF50;
+                color: white;
+            }
+        }
     }
 </style>
